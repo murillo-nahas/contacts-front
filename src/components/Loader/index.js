ca77bb080f51f5664287dd27b842945e61b56707
murillo.nahas <murillonahas@hotmail.com>
2023-05-23 15:59:47 -0300

    feat(refactor): refactoring loader component

--- src/components/Loader/index.js
+++ src/components/Loader/index.js
@@ -3,13 +3,15 @@ import PropTypes from "prop-types";
 
 import { Overlay } from "./styles";
 
+import Spinner from "../Spinner";
+
 export default function Loader({ isLoading }) {
     if (!isLoading) {
         return null;
     }
     return ReactDOM.createPortal(
         <Overlay>
-            <div className="loader" />
+            <Spinner size={90} />
         </Overlay>,
         document.getElementById("loader-root")
     );
