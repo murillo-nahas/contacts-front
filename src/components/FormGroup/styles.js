fb15ee2eae294c3644fc2d04373a267d928570bb
murillo.nahas <murillonahas@hotmail.com>
2023-05-23 16:00:54 -0300

    feat(style): some style changes

--- src/components/FormGroup/styles.js
+++ src/components/FormGroup/styles.js
@@ -11,4 +11,14 @@ export const Container = styled.div`
         display: block;
         color: ${({ theme }) => theme.colors.danger.main};
     }
+
+    .form-item {
+        position: relative;
+    }
+
+    .loader {
+        position: absolute;
+        right: 16px;
+        top: 18px;
+    }
 `;
