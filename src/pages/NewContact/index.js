840ad73098edb50c13e801cd1a231a9eacdc7ac1
murillo.nahas <murillonahas@hotmail.com>
2023-05-25 11:51:08 -0300

    feat: toast eventManager

--- src/pages/NewContact/index.js
+++ src/pages/NewContact/index.js
@@ -1,6 +1,7 @@
 import PageHeader from "../../components/PageHeader";
 import ContactForm from "../../components/ContactForm";
 import ContactsService from "../../services/ContactsService";
+import toast from "../../utils/toast";
 
 export default function NewContact() {
     async function handleSubmit(formData) {
@@ -11,9 +12,17 @@ export default function NewContact() {
                 phone: formData.phone,
                 category_id: formData.categoryId,
             };
-            const response = await ContactsService.createContact(contact);
+            await ContactsService.createContact(contact);
+
+            toast({
+                type: "success",
+                text: "Contato cadastrado com sucesso!",
+            });
         } catch {
-            console.log("erro");
+            toast({
+                type: "danger",
+                text: "Ocorreu um erro ao cadastrar o contato!",
+            });
         }
     }
 
