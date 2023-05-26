722bc3d229555760e142810b70ad58e2384f10af
murillo.nahas <murillonahas@hotmail.com>
2023-05-26 07:10:42 -0300

    feat: listCategories

--- src/pages/EditContact/index.js
+++ src/pages/EditContact/index.js
@@ -1,11 +1,51 @@
 import PageHeader from "../../components/PageHeader";
 import ContactForm from "../../components/ContactForm";
+import { useParams, useHistory } from "react-router-dom";
+import { useState, useEffect } from "react";
+
+import toast from "../../utils/toast";
+
+import Loader from "../../components/Loader";
+
+import ContactsService from "../../services/ContactsService";
 
 export default function EditContact() {
+    const [isLoading, setIsLoading] = useState(true);
+
+    const history = useHistory();
+
+    const { id } = useParams();
+
+    useEffect(() => {
+        async function loadContact() {
+            try {
+                await ContactsService.getContactById(id);
+
+                setIsLoading(false);
+            } catch {
+                history.push("/");
+                toast({
+                    type: "danger",
+                    text: "Contato não encontrado!",
+                });
+            }
+        }
+
+        loadContact();
+    }, [id, history]);
+
+    function handleSubmit() {
+        console.log("oi");
+    }
+
     return (
         <>
+            <Loader isLoading={isLoading} />
             <PageHeader title="Editar Murillo Nahás" />
-            <ContactForm buttonLabel="Salvar alterações" />
+            <ContactForm
+                buttonLabel="Salvar alterações"
+                onSubmit={handleSubmit}
+            />
         </>
     );
 }
